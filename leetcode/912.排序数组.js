dec2008a251b069acbf68d245b9c0b3b69c60dbe
liu zhenbin <536543269@qq.com>
2020-11-17 11:38:58 +0800

    leetcode 排序

--- leetcode/912.排序数组.js
+++ leetcode/912.排序数组.js
@@ -50,75 +50,69 @@ function mergeSort(arr, left, right) {
   let mid = (left + right) >> 1;
   mergeSort(arr, left, mid);
   mergeSort(arr, mid + 1, right);
-  let temp = [];
   let i = left,
     j = mid + 1;
-  let cur = 0;
+  let temp = [];
+  // 只要一边结束
   while (i <= mid && j <= right) {
-    if (arr[i] < arr[j]) {
-      temp[cur] = arr[i++];
+    if (arr[i] <= arr[j]) {
+      temp.push(arr[i++]);
     } else {
-      temp[cur] = arr[j++];
+      temp.push(arr[j++]);
     }
-    cur++;
   }
-  // 处理左右数组剩下的数据
-  while (i <= mid) temp[cur++] = arr[i++];
-  while (j <= right) temp[cur++] = arr[j++];
+  // 处理剩下的
+  while (i <= mid) temp.push(arr[i++]);
+  while (j <= right) temp.push(arr[j++]);
 
-  // 合并完，拷贝到原来的数组
+  // 修改原来数组的值
   for (let k = 0; k < temp.length; k++) {
-    arr[left + k] = temp[k];
+    arr[k + left] = temp[k];
   }
 }
 
 // 插入排序
 // 思路：把数组的第一个元素当做有序数组，从第二个开始遍历，把数字插入到有序的数组里面，使有序数组保持有序。
 function insertSort(arr) {
-  let len = arr.length;
-  for (let i = 1; i < len; i++) {
-    // 缓存变量
-    const temp = arr[i];
+  for (let i = 1; i < arr.length; i++) {
     let j = i;
-    while (j > 0 && arr[j - 1] > temp) {
-      arr[j] = arr[j - 1]; // 后移
+    let temp = arr[i];
+    // 记住这里要用j-1，也就是用上一个跟当前进行比较
+    while (j >= 0 && arr[j - 1] > temp) {
+      arr[j] = arr[j - 1];
       j--;
     }
     arr[j] = temp;
   }
 }
-function partition(arr, start, end) {
-  let pivotPos = start;
-  let pivot = arr[start];
-  for (let i = start + 1; i <= end; i++) {
+
+function partition(arr, left, right) {
+  let pivotPos = left;
+  let pivot = arr[left];
+  for (let i = left + 1; i <= right; i++) {
     if (arr[i] < pivot) {
       pivotPos++;
-      swap(arr, i, pivotPos);
+      swap(arr, pivotPos, i);
     }
   }
-  swap(arr, start, pivotPos);
+  swap(arr, left, pivotPos);
   return pivotPos;
 }
 // 快排
 // 思路：选取一个分界点，把整个数组分为左右两侧，左边的元素小于等于分界点，右边的元素大于分界点，然后把分界点移到中间位置，对左右子数组分别进行递归
-function quickSort(arr) {
-  function quickSortEx(arr, start, end) {
-    if (start < end) {
-      const pos = partition(arr, start, end);
-      quickSortEx(arr, start, pos - 1);
-      quickSortEx(arr, pos + 1, end);
-    }
-    return [arr[end]];
-  }
-  return quickSortEx(arr, 0, arr.length - 1);
+function quickSort(arr, left, right) {
+  if (left >= right) return;
+  const pos = partition(arr, left, right);
+  quickSort(arr, left, pos - 1);
+  quickSort(arr, pos + 1, right);
 }
 
 var sortArray = function (nums) {
-  //   selectSort(nums);
-  //   insertSort(nums);
-  quickSort(nums);
-  //   bubbleSort(nums);
-  //   mergeSort(nums, 0, nums.length - 1);
+  // selectSort(nums);
+  // insertSort(nums);
+  // quickSort(nums, 0, nums.length - 1);
+  // bubbleSort(nums);
+  mergeSort(nums, 0, nums.length - 1);
   return nums;
 };
 // @lc code=end

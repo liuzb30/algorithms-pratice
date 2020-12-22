0305298ff77bd23c239b6b1bb0b4f5719f227c98
liu zhenbin <536543269@qq.com>
2020-12-22 14:17:33 +0800

    快排思路

--- sort/quickSort.js
+++ sort/quickSort.js
@@ -32,6 +32,44 @@ function insertSort(arr) {
   }
 }
 
+// 快排的特点是分治，所谓分治，顾名思义，就是分而治之，将一个复杂的问题，分层两个或两个以上的子问题，再把子问题分成更小的子问题，直到更小的子问题可以简单求解，求解子问题，则原问题的解为子问题解的合并。
+
+// 快排的过程简单的说只有三步：
+// 1. 首先从序列中选取一个基准点
+// 2. 将比这个基准点小的数全部放到它的左边，比这个基准点大的数字放到它的右边
+// 3. 然后分别对左边和右边的数组重复以上的操作，直到数组完全排序
+
+// 具体按以下步骤实现
+// 1. 创建两个指针分别指向最左端和最右端
+// 2.从序列中随机选取一个基准点
+// 3. 左指针往右走，遇到比基准点大则停止
+// 4. 右指针往左走，遇到比基准点小则停止，交换左右指针元素
+// 5. 重复3,4，直到左指针大于右指针。此时，基准点左边的值都比基准点小，右边的值都比基准点大
+// 6. 最后对基准点左右两边的重复以上操作，直到数组完全排序。
+
+function partition(arr, start, end){
+  let left = start
+  let right = end
+  const pivot = arr[Math.floor(Math.random()*(end-start+1)+start)]
+  while(left<=right){
+    while(arr[left]<pivot){
+      left++
+    }
+    while(arr[right]>pivot){
+      right++
+    }
+    if(i<=j){
+      swap(arr,i,j)
+      i+=1
+      j-=1
+    }
+  }
+  return left
+}
+function swap(arr,j,j){
+  [arr[i],arr[j]] = [arr[j],arr[i]]
+}
+
 function quickSort(arr) {
   function quickSortEx(arr, start, end) {
     if (start < end) {
@@ -45,8 +83,8 @@ function quickSort(arr) {
   return quickSortEx(arr, 0, arr.length - 1);
 }
 
-// var arr = [7, 2, 8, 1, 4, 6, 9, 3];
-// insertSort(arr);
-// console.log(arr);
+var arr = [7, 2, 8, 1, 4, 6, 9, 3];
+insertSort(arr);
+console.log(arr);
 
 module.exports = quickSort;
